9d226dfae2241ca5b1af9dbb997c312156dbfa12
ValidCoder <mikedh.prive@gmail.com>
2023-05-24 15:36:36 +0200

    Ez pz project added img (lol ez speedrun)

---
+++ content/content.php
@@ -0,0 +1,7 @@
+<?php
+
+$img = "quandile.jpg";
+$name = "Quandile";
+$subname = "Dingle";
+
+?>
---
+++ content/index.php
@@ -0,0 +1,20 @@
+<?php
+require "content.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <h1><?php echo $name; ?></h1>
+    <h2><?php echo $subname; ?></h2>
+
+    <img src="<?php echo $img; ?>" alt="Gehenkt">
+<body>
+    
+</body>
+</html>
